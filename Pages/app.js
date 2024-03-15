97854ea04db92b6848f60d1f92e57400a9c14515
Gav-31 <163502145+Gav-31@users.noreply.github.com>
2024-03-15 10:28:43 +0400

    Add files via upload

---
+++ Pages/app.js
@@ -0,0 +1,76 @@
+/* Menu Button Script */
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+
+/* Slider Script */
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+//vehicle-navbar
+
+document.addEventListener("DOMContentLoaded", function() {
+    showContent(1); // Display content for SUVs when the page loads
+  });
+  
+function showContent(contentId) {
+    // Hide all vehicle content
+    var vehicleContents = document.querySelectorAll('.content');
+    vehicleContents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+
+    // Show the selected vehicle content
+    var selectedContent = document.getElementById('content-' + Id);
+    if (selectedContent){
+        selectedContent.style.display = 'block';
+    }
+}
